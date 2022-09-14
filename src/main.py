b8f9c13c5cca82fd0f1913354ffe07facd8d74a7
root <root@DESKTOP-MH82SGB.localdomain>
2022-09-14 10:40:52 +0200

    Cleaning of main.py.

--- src/main.py
+++ src/main.py
@@ -2,50 +2,74 @@
 """
 Created on Thu Sep  8 16:07:07 2022
 
-@author: jeand
+@author: Jean Delhomme
+
+Main program for the embedding_project.
+
+This program is performing global, local or glocal protein alignment using
+fasta and embedding files.
+
+Users should follow the instructions described in the README.md file attached
+to the embedding_project repository.
+
 """
-# Utiliser import sys et sys.argv pour gérer les arguments que l'on place dans 
-# le main.
-
-# try et except pour les messages d'errur.Voir errur complémentaire. 
-# cf ch19 et 21.
-
 # ligne de commande 
 # python3 main.py 5_3_exonuclease_1bgxt.t5emb 5_3_EXONUCLEASE_1BGXT.fasta 6PF2K_1bif.t5emb 6PF2K_1BIF.fasta nw
 # python3 main.py adk_2ak3a.t5emb ADK_2AK3A.fasta 6PF2K_1bif.t5emb 6PF2K_1BIF.fasta nw
 
+# Importation of common modules.
 import sys
 import numpy as np
 
+# Importation of the modules used for reading the data files.
 import embedding_reader as er
 import fasta_reader as fr
 
+# Importation of the modules used for the alignment.
 import alignment_matrix as am
 import alignment_algorithm as aa
 
 
 
 if __name__ == "__main__":
-    
+
+###############################################################################
+#                                                                             #
+#                            data preparation                                 #
+#                                                                             #
+###############################################################################
+
+    # Variables for the path of the data files.
     path_embedding = "../data/emb/"
     path_fasta = "../data/fasta/"
     
+    # Variables containing the arguments given when lauching the program.
     embedding_file1 = path_embedding + sys.argv[1]
     fasta_file1 = path_fasta + sys.argv[2]
     embedding_file2 = path_embedding + sys.argv[3]
     fasta_file2 = path_fasta + sys.argv[4]
     
-
+    # Creation of embedding array for each protein.
     embedding1 = er.embedding_reader(embedding_file1)
     embedding2 = er.embedding_reader(embedding_file2)
     
+    # Creation of fasta sequences for each protein.
     fasta1 = fr.fasta_reader(fasta_file1)
     fasta2 = fr.fasta_reader(fasta_file2)
     
+    # Creation of names for each protein.
     prot_name1 = fr.fasta_name(fasta_file1)
     prot_name2 = fr.fasta_name(fasta_file2)
     
+    # Calculation of the dot_product between each embedding at each position
+    # and construction of an array with those dot_products.
     dot_matrix = np.dot(embedding1, embedding2.T)
+  
+###############################################################################
+#                                                                             #
+#                                alignments                                   #
+#                                                                             #
+###############################################################################
     
     # For Needleman and Wunsch (global alignment).
     if len(sys.argv) < 6 or sys.argv[5] == "nw":
@@ -72,10 +96,3 @@ if __name__ == "__main__":
         alignment_matrix = am.alignment_matrix_sw(dot_matrix)  
         # Finds the best path and thus, the sequence alignment.
         aa.glocal(fasta1, fasta2, prot_name1, prot_name2, alignment_matrix)
-        
-    
-    """
-    # ajouter un output sous forme de fichier.
-    # ajouter environnement conda.
-    # faire les push github.
-    """
